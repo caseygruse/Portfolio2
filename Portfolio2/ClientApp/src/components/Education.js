2911e2af6119f63d55935eaa6a6f85756563ee81
caseygruse1 <caseygruse1@gmail.com>
2019-02-20 19:38:04 -0800

    built my own component and hooked it up to app.js

---
+++ Portfolio2/ClientApp/src/components/Education.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Education extends Component {
+    
+    static displayName = Education.name;
+
+    render() {
+        return (
+            <div>
+                <h1> Education Test!!! </h1>
+            </div>
+        );
+    }
+}
