6484e2a0a1e8ab1bad2505ee09985f1a98fbbd6f
caseygruse1 <caseygruse1@gmail.com>
2019-02-28 12:21:43 -0800

    Home page custome navigation

---
+++ Portfolio2/ClientApp/src/components/About.js
@@ -0,0 +1,15 @@
+﻿import React, { Component } from 'react';
+
+export class About extends Component {
+
+    static displayName = About.name;
+
+    render() {
+        return (
+            <div>
+                <h1> Education </h1>
+                <h3 id="summary"> Summary</h3>
+            </div>
+        );
+    }
+}
--- Portfolio2/ClientApp/src/components/Home.js
+++ Portfolio2/ClientApp/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { OpeningStatement } from './OpeningStatement';
-
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -15,7 +15,21 @@ export class Home extends Component {
                 <div id="firstLast">
                     <h1 class="name">Casey</h1>
                     <h2 class="name">Gruse</h2>      
-                </div> 
+                  </div> 
+
+                  <div class="row">
+                      <div class="col-lg-4 col-xs-12">
+                          <div class="left" tag={Link} to="/">Home</div>
+                      </div>
+                      <div class="col-lg-4 col-xs-12">
+                          <div class="middle" tag={Link} to="/Education">Education</div>
+                      </div>
+                      <div class="col-lg-4 col-xs-12">
+                          <div class="right" tag={Link}  to="/About">About</div>
+                      </div>
+                      
+                  </div>
+
             </div>
         </div>
           
