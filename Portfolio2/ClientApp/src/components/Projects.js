6e1d4ec3c562991b36942815488d442a0e8fabc9
caseygruse1 <caseygruse1@gmail.com>
2019-03-21 15:57:30 -0700

    Use a generic onlclick to fire another method that can take perameters.

--- Portfolio2/ClientApp/src/components/Projects.js
+++ Portfolio2/ClientApp/src/components/Projects.js
@@ -4,13 +4,18 @@ import './Css/Projects.css';
 // Child Components
 import { PageHeader } from './Shared/PageHeader';
 
+let contents;
+
 export class Projects extends Component {
     //static displayName = Projects.name;
     constructor(props) {
         super(props);
         this.state = {
             projects: [],
+            singleProject: [],
         };
+        this.displayProjectInformation = this.displayProjectInformation.bind(this);
+        this.displayProjectNames = this.displayProjectNames.bind(this);
     }
 
     // -------------------------
@@ -45,18 +50,26 @@ export class Projects extends Component {
     //    return projectRows;
     //}
 
+    ///displays project names 
+    ///loops through projects and displays names  
     displayProjectNames(projects) {
         const projectNames = [];
-
+        
         projects.forEach((project) => {
             projectNames.push(
-                <div className="col-lg-4 centerNames">
-                    <p>{project.name}</p>
-                </div>
+                <div key={project.projectId} className="col-lg-4 centerNames">
+                    <button onClick={this.displayProjectInformation(projects, project.name)}> {project.name} </button>                          
+                </div>               
             )
         })
+
+        //https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
+
         return projectNames;
     }
+    //pass btn value by using this.value
+
+    ///TO DO:  When project name is clicked display all project properites below project names.
 
     render() {
         console.log('RENDERING')
@@ -69,7 +82,8 @@ export class Projects extends Component {
                     <div className="row">
                         {this.displayProjectNames(this.state.projects)}
                     </div>
-                    
+
+                    {contents}
 
                     {/*{this.buildProjectRows(this.state.projects)} */}
                 </div>
